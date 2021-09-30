1304d7a5b330247620f071c132eb0507c6394aa9
Stefan <steve232np@gmail.com>
2021-09-30 19:11:24 +0200

    add jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,19 @@ module.exports = {
     browser: true,
     es2021: true,
     node: true,
+    'jest/globals': true,
   },
   extends: [
     'plugin:vue/recommended',
     'airbnb-base',
+    'plugin:jest/recommended',
+    'plugin:jest/style',
   ],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
+  plugins: ['jest'],
   settings: {
     'import/resolver': {
       webpack: {
---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  moduleFileExtensions: ["js", "json", "vue"],
+  transform: {
+    "^.+\\.js$": "babel-jest",
+    "^.+\\.vue$": "vue-jest"
+  },
+  testEnvironment: 'jsdom'
+}
