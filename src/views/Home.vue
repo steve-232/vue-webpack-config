1a980b84fbf6c9f0aeba9e0e2238d6b2963e141a
Stefan <steve232np@gmail.com>
2021-10-09 13:08:42 +0200

    feat: add axios

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,13 +2,25 @@
   <div>
     <p>Home</p>
     <Paragraph>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Paragraph>
-    <Button :on-click="testEvent">
-      Btn
+    <ul v-if="users.length">
+      <li
+        v-for="user in users"
+        :key="user.id"
+      >
+        {{ user.username }} - {{ user.name }} - {{ user.email }} - {{ user.phone }}
+      </li>
+    </ul>
+    <p v-else-if="errInReq.length">
+      {{ errInReq }}
+    </p>
+    <Button :on-click="fetchUsers">
+      Fetch Users
     </Button>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 import Paragraph from '@/components/Paragraph';
 import Button from '@/components/Button';
 
@@ -17,10 +29,25 @@ export default {
     Paragraph,
     Button,
   },
+  data() {
+    return {
+      users: [],
+      errInReq: '',
+    };
+  },
   methods: {
-    testEvent() {
-      // eslint-disable-next-line no-console
-      console.log('button click');
+    fetchUsers() {
+      axios.get('https://jsonplaceholder.typicode.com/users')
+        .then((res) => {
+          if (res.status === 200) {
+            this.users = res.data;
+          } else {
+            this.errInReq = 'Users are not available';
+          }
+        })
+        .catch(() => {
+          this.errInReq = 'Error! Users are not available';
+        });
     },
   },
 };
