d540a3a54e9c7b010891a49403d41d15e7002518
Stefan <steve232np@gmail.com>
2021-10-12 18:38:45 +0200

    fix: correctly resolve "@" inside of test functions

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,8 @@ module.exports = {
     '^.+\\.vue$': 'vue-jest',
   },
   testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
   setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
 };
